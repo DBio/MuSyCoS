f83758692553e154d962f7ef96236c5aec72c3b7
Adam Streck <adam.streck@gmail.com>
2014-02-05 15:03:52 +0100

    Semantic control added.

--- model/model_parsers.hpp
+++ model/model_parsers.hpp
@@ -7,27 +7,66 @@
 #include "model.hpp"
 
 namespace ModelParsers {
-	const string spec_name("[[:alpha:]](\\w)*");
-	const string assign("'=");
-	const string ext_literal(spec_name + "(\\{\\d+(,\\d+)*\\})?");
-	const string clause("(\\d*\\*)?" + ext_literal + "(\\*" + ext_literal + ")*");
-	const string rule(spec_name + assign + clause + "(\\+" + clause + ")*");
+	const string spec_name_form("([[:alpha:]]\\w*)"); 
+	const string assign_form("'=");
+	const string ext_literal_form(spec_name_form + "(\\{\\d+(,\\d+)*\\})?");
+	const string clause_from("((\\d*\\*)?" + ext_literal_form + "(\\*" + ext_literal_form + ")*)");
+	const string line_form(spec_name_form + assign_form + clause_from + "(\\+" + clause_from + ")*");
 
 	void control_syntax(const vector<string> & lines) {
-		regex control{ rule };
+		regex control{ line_form };
 		for (const size_t i : crange(0u, lines.size())) 
 			if (!regex_match(lines[i], control))
 				throw runtime_error("Line " + to_string(i) + ": invalid format \"" + lines[i] + "\"");
 	}
 
-	Specie obtainSpecie(string line) {
-		Specie result;
+	const string num_literal_form = "(\\d+)[^\\},]";
+	int findMaxNumLiteral(string line) {
+		smatch clause_matches;
+		vector<string> string_literals;
+		// Obtain all the clauses (only the whole match).
+		while (regex_search(line, clause_matches, regex(num_literal_form))) {
+			string_literals.emplace_back(clause_matches[1]); // Capture the first (and only) submatch
+			line = clause_matches.suffix().str();
+		}
+		if (string_literals.empty())
+			return 1;
 
-		return result;
+		vector<int> numeric_literals(string_literals.size());
+		rng::transform(string_literals, numeric_literals.begin(), [](const string & s) {return stoi(s); });
+		return *rng::max_element(numeric_literals);
+	}
+
+	const string rule_form = "=(.*)";
+	Specie obtainSpecie(string line) {	
+		smatch match_name;
+		regex_search(line, match_name, regex(spec_name_form));
+		smatch match_rule;
+		regex_search(line, match_rule, regex(rule_form));
+
+		return{ match_name[0].str(), findMaxNumLiteral(line), match_rule[1].str() };
 	}
 
 	void control_semantics(const vector<Specie> & species) {
+		for (const Specie specie : species) {
+			string specie_rule = specie.rule;
+			smatch match_clauses;
+			while (regex_search(specie_rule, match_clauses, regex(ext_literal_form))) {
+				string current_specie = match_clauses[1];
+				auto tested = rng::find_if(species, [&current_specie](const Specie & spec) {return spec.name.compare(current_specie) == 0; });
+				if (tested == species.end())
+					throw runtime_error("Specie \"" + current_specie + "\" not found in the rule \"" + specie.rule + "\"");
 
+				smatch match_numbers;
+				string current_numbers = match_clauses[2];
+				while (regex_search(current_numbers, match_numbers, regex("\\d+"))) {
+					if (stoi(match_numbers[0]) > tested->max_val)
+						throw invalid_argument("Value " + match_numbers[0].str() + " is greater than maximal derived value of the specie " + tested->name);
+					current_numbers = match_numbers.suffix().str();
+				}
+				specie_rule = match_clauses.suffix().str();
+			}
+		}
 	}
 };
 
--- test/parsing_test.hpp
+++ test/parsing_test.hpp
@@ -9,16 +9,21 @@
 #include "../model/model_parsers.hpp"
 
 TEST(ParstingTest, RegexSyntax) {
-	EXPECT_TRUE(regex_match("A2_a", regex(ModelParsers::spec_name)));
-	EXPECT_FALSE(regex_match("2a", regex(ModelParsers::spec_name)));
+	EXPECT_TRUE(regex_match("A2_a", regex(ModelParsers::spec_name_form)));
+	EXPECT_FALSE(regex_match("2a", regex(ModelParsers::spec_name_form)));
 
-	EXPECT_TRUE(regex_match("A2_a", regex(ModelParsers::ext_literal)));
-	EXPECT_TRUE(regex_match("A{21}", regex(ModelParsers::ext_literal)));
-	EXPECT_TRUE(regex_match("A{0,2}", regex(ModelParsers::ext_literal)));
-	EXPECT_FALSE(regex_match("A{}", regex(ModelParsers::ext_literal)));
+	EXPECT_TRUE(regex_match("A2_a", regex(ModelParsers::ext_literal_form)));
+	EXPECT_TRUE(regex_match("A{21}", regex(ModelParsers::ext_literal_form)));
+	EXPECT_TRUE(regex_match("A{0,2}", regex(ModelParsers::ext_literal_form)));
+	EXPECT_FALSE(regex_match("A{}", regex(ModelParsers::ext_literal_form)));
 
-	EXPECT_TRUE(regex_match("A*B", regex(ModelParsers::clause)));
-	EXPECT_TRUE(regex_match("2*A*B", regex(ModelParsers::clause)));
-	EXPECT_TRUE(regex_match("A{0,2}*AB0{1}", regex(ModelParsers::clause)));
-	EXPECT_FALSE(regex_match("A*2", regex(ModelParsers::clause)));
+	EXPECT_TRUE(regex_match("A*B", regex(ModelParsers::clause_from)));
+	EXPECT_TRUE(regex_match("2*A*B", regex(ModelParsers::clause_from)));
+	EXPECT_TRUE(regex_match("A{0,2}*AB0{1}", regex(ModelParsers::clause_from)));
+	EXPECT_FALSE(regex_match("A*2", regex(ModelParsers::clause_from)));
+	EXPECT_FALSE(regex_match("", regex(ModelParsers::clause_from)));
+
+	EXPECT_TRUE(regex_match("A'=A", regex(ModelParsers::line_form)));
+	EXPECT_TRUE(regex_match("B_d5'=2*Ca2{1,5}+3*aP", regex(ModelParsers::line_form)));
+	EXPECT_FALSE(regex_match("B=B", regex(ModelParsers::line_form)));
 }
